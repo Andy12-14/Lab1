15086e033b900c0bedeeaadfd52291809202e53d
Marcyvan1777 <marc.metodakou @aivancity.education>
2025-10-22 22:10:36 +0200

    add divide and multiply functions

--- src/utils.py
+++ src/utils.py
@@ -10,4 +10,28 @@ def subtract(*numbers):
     result = numbers[0]
     for num in numbers[1:]:
         result -= num
-    return result
+    return result 
+
+
+def multiply(*numbers):
+    """multiply an unlimted number of parameters"""
+    L=list(numbers)
+    P=1
+    for elt in L:
+        P*=elt
+    return P 
+
+def divide(*numbers):
+    """divide an unlimted number of parameters"""
+    L=list(numbers)
+    D=L[0]
+    for elt in L[1:]:
+        if elt==0:
+            raise ZeroDivisionError("can not divide by zero")
+        D/=elt
+    return D 
+
+
+
+
+
